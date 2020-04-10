434b601803c73285500714d8fac43ab2e8fa5cb7
su600 <su600@live.com>
2020-04-10 11:20:17 +0800

    uodate README

--- InstapaperBookmarks.py
+++ InstapaperBookmarks.py
@@ -8,24 +8,44 @@ I.login("su600@live.com", "greedisgood1")
 # b = instapaper.Bookmark(I, {"url": "http://su600.cn"})
 # b.save()
 
+# # 读取10条记录
 # c = I.bookmarks(limit=10)
-# print(c.title)
 # for a in c:
 #     print(a.title)
 #     print(a.url)
 
+# # 存档最新一条
+# d = I.bookmarks(limit=1)
+# s=d[0].bookmark_id
+# print(s)
+# d = instapaper.Bookmark(I, {'bookmark_id': s})
+# d.archive()
+# # 如何区分不同标签的删除按钮 submit todo
+
 app = Flask(__name__)
 
 @app.route("/",methods=['GET', 'POST'])
-def home():
+def home(): ## 打开和刷新列表
     # c = I.bookmarks(limit=10)
     if request.method=="GET":
         # print("sssssssssssssssssss")
         c = I.bookmarks(limit=10)
-    # elif request.method == "POST":
-    #     print("sssssssssssssssssss")
-    #     c=I.delete()
-    #     c = I.bookmarks(limit=10)
+    
+    if request.method == "POST": #存档该条记录（非删除）
+
+        a=request.form.to_dict()
+        s = a["id"]
+        d = instapaper.Bookmark(I, {'bookmark_id': s})
+        # print(s)
+        # print(a["Action"])
+        if a["Action"] == "Archive":
+            # print("存档")
+            d.archive()
+            c = I.bookmarks(limit=10)
+        if a["Action"] == "Delete":
+            # print("删除")
+            d.delete()
+            c = I.bookmarks(limit=10)
     return render_template("instapaper.html",list=c)
 
 app.run("0.0.0.0",port=1000)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # InstapaperChrome
 InstapaperChromeExtension
+
+Get bookmark urls from Instapaper, Archive or Delete.
+
+* Together with Instapaper App:
+  
+    ![iPhone](https://raw.githubusercontent.com/su600/InstapaperChrome/iphone.jpg)
+
+* It's very useful to sync Readlist between iPhone/Android and Chrome like this:
+
+    ![Chrome](https://raw.githubusercontent.com/su600/InstapaperChrome/chrome.png)
+
+> Note: beta version, Python program running on Aliyun ECS, just for me.
+>   Su600.cn:1000
+
